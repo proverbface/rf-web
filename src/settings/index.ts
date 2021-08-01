f00946eac9d69506584afc06d817bb24da341c12
JP Proverbio <jp.proverbio@idealgap.co.nz>
2021-08-01 20:03:22 +1200

    Updates

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,5 +1,6 @@
 export default {
   isLocal: process.env.VUE_APP_IS_LOCAL === '1',
   apiUrlRest: process.env.VUE_APP_apiUrlRest,
+  itemApiUrl: 'http://127.0.0.1:4000/items',
   gcpPublicBucketUri: process.env.VUE_APP_gcpPublicBucketUri,
 };
